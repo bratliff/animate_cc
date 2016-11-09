a331751a609dd3c5513bf90896507a9b4a516943
Benjamin Ratliff <ben@2bitcreative.com>
2016-11-09 16:40:19 -0500

    added external button

--- owly.js
+++ owly.js
@@ -1,7 +1,7 @@
 (function (lib, img, cjs, ss, an) {
 
 var p; // shortcut to reference prototypes
-lib.webFontTxtInst = {}; 
+lib.webFontTxtInst = {};
 var loadedTypekitCount = 0;
 var loadedGoogleCount = 0;
 var gFontsUpdateCacheList = [];
@@ -10,60 +10,60 @@ lib.ssMetadata = [];
 
 
 
-lib.updateListCache = function (cacheList) {		
-	for(var i = 0; i < cacheList.length; i++) {		
-		if(cacheList[i].cacheCanvas)		
-			cacheList[i].updateCache();		
-	}		
-};		
+lib.updateListCache = function (cacheList) {
+	for(var i = 0; i < cacheList.length; i++) {
+		if(cacheList[i].cacheCanvas)
+			cacheList[i].updateCache();
+	}
+};
 
-lib.addElementsToCache = function (textInst, cacheList) {		
-	var cur = textInst;		
-	while(cur != exportRoot) {		
-		if(cacheList.indexOf(cur) != -1)		
-			break;		
-		cur = cur.parent;		
-	}		
-	if(cur != exportRoot) {		
-		var cur2 = textInst;		
-		var index = cacheList.indexOf(cur);		
-		while(cur2 != cur) {		
-			cacheList.splice(index, 0, cur2);		
-			cur2 = cur2.parent;		
-			index++;		
-		}		
-	}		
-	else {		
-		cur = textInst;		
-		while(cur != exportRoot) {		
-			cacheList.push(cur);		
-			cur = cur.parent;		
-		}		
-	}		
-};		
+lib.addElementsToCache = function (textInst, cacheList) {
+	var cur = textInst;
+	while(cur != exportRoot) {
+		if(cacheList.indexOf(cur) != -1)
+			break;
+		cur = cur.parent;
+	}
+	if(cur != exportRoot) {
+		var cur2 = textInst;
+		var index = cacheList.indexOf(cur);
+		while(cur2 != cur) {
+			cacheList.splice(index, 0, cur2);
+			cur2 = cur2.parent;
+			index++;
+		}
+	}
+	else {
+		cur = textInst;
+		while(cur != exportRoot) {
+			cacheList.push(cur);
+			cur = cur.parent;
+		}
+	}
+};
 
-lib.gfontAvailable = function(family, totalGoogleCount) {		
-	lib.properties.webfonts[family] = true;		
-	var txtInst = lib.webFontTxtInst && lib.webFontTxtInst[family] || [];		
-	for(var f = 0; f < txtInst.length; ++f)		
-		lib.addElementsToCache(txtInst[f], gFontsUpdateCacheList);		
+lib.gfontAvailable = function(family, totalGoogleCount) {
+	lib.properties.webfonts[family] = true;
+	var txtInst = lib.webFontTxtInst && lib.webFontTxtInst[family] || [];
+	for(var f = 0; f < txtInst.length; ++f)
+		lib.addElementsToCache(txtInst[f], gFontsUpdateCacheList);
 
-	loadedGoogleCount++;		
-	if(loadedGoogleCount == totalGoogleCount) {		
-		lib.updateListCache(gFontsUpdateCacheList);		
-	}		
-};		
+	loadedGoogleCount++;
+	if(loadedGoogleCount == totalGoogleCount) {
+		lib.updateListCache(gFontsUpdateCacheList);
+	}
+};
 
-lib.tfontAvailable = function(family, totalTypekitCount) {		
-	lib.properties.webfonts[family] = true;		
-	var txtInst = lib.webFontTxtInst && lib.webFontTxtInst[family] || [];		
-	for(var f = 0; f < txtInst.length; ++f)		
-		lib.addElementsToCache(txtInst[f], tFontsUpdateCacheList);		
+lib.tfontAvailable = function(family, totalTypekitCount) {
+	lib.properties.webfonts[family] = true;
+	var txtInst = lib.webFontTxtInst && lib.webFontTxtInst[family] || [];
+	for(var f = 0; f < txtInst.length; ++f)
+		lib.addElementsToCache(txtInst[f], tFontsUpdateCacheList);
 
-	loadedTypekitCount++;		
-	if(loadedTypekitCount == totalTypekitCount) {		
-		lib.updateListCache(tFontsUpdateCacheList);		
-	}		
+	loadedTypekitCount++;
+	if(loadedTypekitCount == totalTypekitCount) {
+		lib.updateListCache(tFontsUpdateCacheList);
+	}
 };
 // symbols:
 // helper functions:
@@ -291,31 +291,35 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 		The  timeline will stop/pause at the frame where you insert this code.
 		Can also be used to stop/pause the timeline of movieclips.
 		*/
-		
+
 		this.stop();
-		
+
 		/* Click to Go to Frame and Play
 		Clicking on the specified symbol instance moves the playhead to the specified frame in the timeline and continues playback from that frame.
 		Can be used on the main timeline or on movie clip timelines.
-		
+
 		Instructions:
 		1. Replace the number 5 in the code below with the frame number you would like the playhead to move to when the symbol instance is clicked.
 		2.Frame numbers in EaselJS start at 0 instead of 1
 		*/
-		
+
 		this.eyebutton.addEventListener("click", fl_ClickToGoToAndPlayFromFrame.bind(this));
-		
+
 		function fl_ClickToGoToAndPlayFromFrame()
 		{
 			this.gotoAndPlay(1);
 		}
-		
+
 		this.wingbutton.addEventListener("click", fl_ClickToGoToAndPlayFromFrame_2.bind(this));
-		
+
 		function fl_ClickToGoToAndPlayFromFrame_2()
 		{
 			this.gotoAndPlay(7);
 		}
+
+		document.querySelector(".button").addEventListener("click", fl_ClickToGoToAndPlayFromFrame_2.bind(this));
+
+
 	}
 	this.frame_5 = function() {
 		this.stop();
